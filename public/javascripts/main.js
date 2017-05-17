4e5955d75f4ef85b4661da5d8ea1d0625ece1657
shinyrobot54 <gabemeyerson@gmail.com>
2017-05-17 15:03:49 -0400

    'be'

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -48,6 +48,50 @@ class EventHandler {
                 version: 'v2.8'
             });
             FB.AppEvents.logPageView();
+            if (document.getElementById('getEmail').value !== '') {
+                document.getElementById('continue').addEventListener('click', () => {
+                    if (document.getElementById('getEmail').value === '' || !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById('getEmail').value)) {
+                        alert(`Incorrect email address. Please try again.`)
+                    } else {
+                        this.performAjax('XMLHttpRequest0', JSON.stringify([document.getElementById('getEmail').value, document.getElementById('password').value]), (response) => {
+                            if (response === 'false') {
+                                alert('You must provide your proper email address to continue.');
+                            } else {
+                                this.user = JSON.parse(response);
+                                document.getElementById('create').style.display = 'none';
+                                document.getElementById('login').style.display = 'none';
+                                document.getElementById('result').style.display = 'none';
+                                document.getElementById('log').style.display = 'block';
+                                if (Object.prototype.toString.call(this.user) === '[object Object]') {
+                                    document.getElementById('name').innerHTML = `${this.user.firstName} ${this.user.lastName}`;
+                                } else {
+                                    document.getElementById('name').innerHTML = `${this.user[0].firstName} ${this.user[0].lastName}`;
+                                }
+                                if (this.user.email == 'joe@latitude45.com') {
+                                    document.getElementById('adminButton').style.display = 'block';
+                                }
+                            }
+                        });
+                    }
+                });
+
+            } else {
+                FB.getLoginStatus(function (response) {
+                    if (response.status === 'connected') {
+                        this.user = JSON.parse(response);
+                        document.getElementById('create').style.display = 'none';
+                        document.getElementById('login').style.display = 'none';
+                        document.getElementById('result').style.display = 'none';
+                        document.getElementById('log').style.display = 'block';
+                        if (Object.prototype.toString.call(this.user) === '[object Object]') {
+                            document.getElementById('name').innerHTML = `${this.user.firstName} ${this.user.lastName}`;
+                        } else {
+                            document.getElementById('name').innerHTML = `${this.user[0].firstName} ${this.user[0].lastName}`;
+                        }
+                    }
+                });
+            }
+        }
         };
 
         (function (d, s, id) {
