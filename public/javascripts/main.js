81584c6ba0b28ebba262799c612135c476205554
shinyrobot54 <gabemeyerson@gmail.com>
2017-05-17 14:57:53 -0400

    'death'

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -81,33 +81,35 @@ class EventHandler {
     }
 
     handleContinue() {
-        document.getElementById('continue').addEventListener('click', () => {
-            if (document.getElementById('getEmail').value === '' || !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById('getEmail').value)) {
-                alert(`Incorrect email address. Please try again.`)
-            } else {
-                this.performAjax('XMLHttpRequest0', JSON.stringify([document.getElementById('getEmail').value, document.getElementById('password').value]), (response) => {
-                    if (response === 'false') {
-                        alert('You must provide your proper email address to continue.');
-                    } else {
-                        this.user = JSON.parse(response);
-                        document.getElementById('create').style.display = 'none';
-                        document.getElementById('login').style.display = 'none';
-                        document.getElementById('result').style.display = 'none';
-                        document.getElementById('log').style.display = 'block';
-                        if (Object.prototype.toString.call(this.user) === '[object Object]') {
-                            document.getElementById('name').innerHTML = `${this.user.firstName} ${this.user.lastName}`;
+        if (document.getElementById('getEmail').value !== '') {
+            document.getElementById('continue').addEventListener('click', () => {
+                if (document.getElementById('getEmail').value === '' || !/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(document.getElementById('getEmail').value)) {
+                    alert(`Incorrect email address. Please try again.`)
+                } else {
+                    this.performAjax('XMLHttpRequest0', JSON.stringify([document.getElementById('getEmail').value, document.getElementById('password').value]), (response) => {
+                        if (response === 'false') {
+                            alert('You must provide your proper email address to continue.');
                         } else {
-                            document.getElementById('name').innerHTML = `${this.user[0].firstName} ${this.user[0].lastName}`;
+                            this.user = JSON.parse(response);
+                            document.getElementById('create').style.display = 'none';
+                            document.getElementById('login').style.display = 'none';
+                            document.getElementById('result').style.display = 'none';
+                            document.getElementById('log').style.display = 'block';
+                            if (Object.prototype.toString.call(this.user) === '[object Object]') {
+                                document.getElementById('name').innerHTML = `${this.user.firstName} ${this.user.lastName}`;
+                            } else {
+                                document.getElementById('name').innerHTML = `${this.user[0].firstName} ${this.user[0].lastName}`;
+                            }
+                            if (this.user.email == 'joe@latitude45.com') {
+                                document.getElementById('adminButton').style.display = 'block';
+                            }
                         }
-                        if (this.user.email == 'joe@latitude45.com') {
-                            document.getElementById('adminButton').style.display = 'block';
-                        }
-                    }
-                });
-            }
+                    });
+                }
+            });
 
+        } else {
             FB.getLoginStatus(function (response) {
-                statusChangeCallback(response);
                 if (response.status === 'connected') {
                     this.user = JSON.parse(response);
                     document.getElementById('create').style.display = 'none';
@@ -121,8 +123,7 @@ class EventHandler {
                     }
                 }
             });
-
-        });
+        }
     }
 
     handleCreate() {
@@ -252,9 +253,16 @@ class EventHandler {
     }
 }
 
-window.addEventListener('load', () => {
-    new main();
-});
+window
+    .addEventListener(
+        'load'
+        , () => {
+            new
+
+                main();
+        }
+    )
+;
 
 
 /*
