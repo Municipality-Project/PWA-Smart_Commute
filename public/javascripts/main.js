38abcba7655c9577fcaf7ae53e168b7b3ff4b06f
shinyrobot54 <gabemeyerson@gmail.com>
2017-05-19 14:14:00 -0400

    'd'

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -52,16 +52,16 @@ class EventHandler {
 
         };
 
-        // (function (d, s, id) {
-        //     var js, fjs = d.getElementsByTagName(s)[0];
-        //     if (d.getElementById(id)) {
-        //         return;
-        //     }
-        //     js = d.createElement(s);
-        //     js.id = id;
-        //     js.src = "//connect.facebook.net/en_US/sdk.js";
-        //     fjs.parentNode.insertBefore(js, fjs);
-        // }(document, 'script', 'facebook-jssdk'));
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {
+                return;
+            }
+            js = d.createElement(s);
+            js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
         // FB.Event.subscribe('auth.login', (response) => {
         //     let request = new XMLHttpRequest();
         //     request.open("POST", response.authResponse.userID, true);
